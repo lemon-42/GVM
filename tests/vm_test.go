6d53a756cd41d37662e6696a8b41b16665c9b099
lemon_42 <juliendasilva9495@gmail.com>
2024-05-23 09:20:47 +0200

    Add test for push sub function

--- tests/vm_test.go
+++ tests/vm_test.go
@@ -1,1 +1,25 @@
 package tests
+
+import (
+	"gvm/vm"
+	"testing"
+)
+
+// Push/Pop instruction test function
+func TestPushPop(t *testing.T) {
+	v := vm.NewVM(1024, 1)
+
+	v.Push(101)
+	if v.Pop() != 101 {
+		t.Errorf("Expected 101")
+	}
+
+	v.Push(42)
+	v.Push(69)
+	if v.Pop() != 69 {
+		t.Errorf("Expected 69")
+	}
+	if v.Pop() != 42 {
+		t.Errorf("Expected 42")
+	}
+}
